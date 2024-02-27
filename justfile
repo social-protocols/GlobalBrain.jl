36cbd0e55575626c9d5bb1c3fe88ab3d97a56a6e
Jonathan R. Warden <john.warden@gmail.com>
2024-02-27 17:50:49 +0100

    Fix and cleanup sim. Define SOCIAL_PROTOCOLS_DATADIR

--- justfile
+++ justfile
@@ -21,7 +21,7 @@ reset-db:
     sqlite3 $DATABASE_PATH < sql/triggers.sql
 
 sim:
-    julia --project scripts/sim.jl
+    julia --project scripts/sim.jl && sqlite3 $SIM_DATABASE_PATH "select * from score;"
 
 
 ############ TESTS ##############
--- scripts/sim.jl
+++ scripts/sim.jl
@@ -4,12 +4,11 @@ else
     include("../src/GlobalBrainService.jl")
 end
 
-# include("src/ScoreDB/ScoreDB.jl")
 
 using Main.GlobalBrainService
 using Random, Distributions, SQLite
 
-database_path = "data/sim.db"
+database_path = ENV["SIM_DATABASE_PATH"]
 
 function reset_db(path::String = database_path)::SQLite.DB
     if isfile(path)
@@ -18,36 +17,15 @@ function reset_db(path::String = database_path)::SQLite.DB
     return get_score_db(path)
 end
 
-# database_path = "data/sim.db"
-# if isfile(database_path)
-#     println("delete db? (y/n)")
-#     are_you_sure = readline()
-#     if are_you_sure == "y"
-#         rm(database_path)
-#     else
-#         error("okay")
-#     end
-# end
-# db = get_score_db(database_path)
+
+reset_db(database_path)
+db = get_score_db(database_path)
 
 
-# Scenario
-# post: "Did you draw a blue marble?"
-# users vote honestly.
-
-tag_id = 1
-# post_id = 1
-
-
-# n = 1000
-# p = 0.37  # Set the probability parameter for the Bernoulli distribution
-# draws = rand(Bernoulli(p), n)
-
-
-function process_votes(db, draws::Vector{Bool}, post_id, parent_id, vote_event_id::Int = 1)
+function process_votes(db, tag_id, parent_id, post_id, draws::Vector{Bool}, vote_event_id::Int = 1)
     t = 0
 
-    scores = []
+    # scores = []
 
     for (i, draw) in enumerate(draws)
         vote = draw == 1 ? 1 : -1
@@ -63,65 +41,80 @@ function process_votes(db, draws::Vector{Bool}, post_id, parent_id, vote_event_i
             created_at=t
         )
         GlobalBrainService.process_vote_event(db, vote_event) do score
-            push!(scores, score)
+            vote_event_id += 1
+            # push!(scores, score)
         end
-        vote_event_id += 1
     end
-    return scores
+    return vote_event_id
 end
 
-# post_id = 1
-# scores = process_votes(draws, post_id, nothing, 1)
 
-# print score
-# total_error = relative_entropy
+voteEventId = 0
 
+begin
+    tag_id = 1
+    # Scenario
+    # post: "Did you draw a blue marble?"
+    # users vote honestly.
 
-# common priors
-p_a_given_b = .9
-p_a_given_not_b = .01
-p_b = .5
-
-# Law of total probability
-p_a = p_b * p_a_given_b + (1 - p_b) * p_a_given_not_b
+    post_id = 1
 
 
-posterior_b = 1
-posterior_a = p_a_given_b
+    n = 1000
+    p = 0.37  # Set the probability parameter for the Bernoulli distribution
+    draws = rand(Bernoulli(p), n)
+    
+    voteEventId = process_votes(db, tag_id, nothing, post_id, draws, voteEventId+1)
+end
 
-n_users = 100
+begin
+    tag_id = 2
+    # Scenario
+    # Users have common priors wrt a/b
+    # post: is a true
+    # all users answer no because p_a is just under 50%
+    # user U given true value of b
+    # user U posts note saying b is true
 
-root_post_id = 1
-note_id = 2
+    # all users trust user U
+    # minority of users consider U's note and thus form same posterior belief in B
+    # these users also change vote on A accordingly
+    # algorithm should estimate posterior_a close to true posterior_a
 
-db = reset_db(database_path)
-draws_0 = [p_a > 0.5 ? true : false for i in 1:n_users]
 
-scores_0 = process_votes(db, draws_0, root_post_id, nothing)
+    # common priors
+    p_a_given_b = .9
+    p_a_given_not_b = .01
+    p_b = .5
 
-n_subset = 10
-draws_1 = [posterior_b > 0.5 ? true : false for i in 1:n_subset]
-draws_2 = [posterior_a > 0.5 ? true : false for i in 1:n_subset]
+    # Law of total probability
+    p_a = p_b * p_a_given_b + (1 - p_b) * p_a_given_not_b
 
-scores_1 = process_votes(db, draws_1, note_id, root_post_id, scores_0[end].vote_event_id + 1)
-scores_2 = process_votes(db, draws_2, root_post_id, nothing, scores_1[end].vote_event_id + 1)
+
+    posterior_b = 1
+    posterior_a = p_a_given_b
+
+    n_users = 100
+
+    root_post_id = 1
+    note_id = 2
+
+    draws_0 = [p_a > 0.5 ? true : false for i in 1:n_users]
+
+    voteEventId = process_votes(db, tag_id, nothing, root_post_id, draws_0, voteEventId + 1)
+
+    n_subset = 10
+    draws_1 = [posterior_b > 0.5 ? true : false for i in 1:n_subset]
+    draws_2 = [posterior_a > 0.5 ? true : false for i in 1:n_subset]
+
+    voteEventId = process_votes(db, tag_id, root_post_id, note_id, draws_1, voteEventId + 1)
+    voteEventId = process_votes(db, tag_id, nothing, root_post_id, draws_2, voteEventId + 1)
+
+    # look at scores in score table
+end
+
 
 close(db)
-
-
-
-
-# Scenario
-# Users have common priors wrt a/b
-# post: is a true
-# all users answer no because p_a is just under 50%
-# user U given true value of b
-# user U posts note saying b is true
-
-# all users trust user U
-# minority of users consider U's note and thus form same posterior belief in B
-# these users also change vote on A accordingly
-# algorithm should estimate posterior_a close to true posterior_a
 
 
 
