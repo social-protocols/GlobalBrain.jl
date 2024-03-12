99f11574b920845a3c04c300a12d73ca0ce8ca02
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-03-12 17:34:50 +0100

    Refactor simulation 2
    - visualization works
    - framework adapted

--- scripts/sim.jl
+++ scripts/sim.jl
@@ -11,7 +11,7 @@ include("../simulations/sim3-counter-argument.jl")
 db = get_sim_db(ENV["SIM_DATABASE_PATH"]; reset = true)
 
 run_simulation!(marbles, db, tag_id = 1)
-# run_simulation!(b_implies_a, db, tag_id = 2)
+run_simulation!(b_implies_a, db, tag_id = 2)
 # run_simulation!(counter_argument, db, tag_id = 3)
 
 close(db)
--- simulations/sim2-b-implies-a.jl
+++ simulations/sim2-b-implies-a.jl
@@ -1,20 +1,30 @@
-# Scenario
-# Users have common priors wrt a/b
-# post: is a true
-# all users answer no because p_a is just under 50%
-# user U given true value of b
-# user U posts note saying b is true
-
-# all users trust user U
-# minority of users consider U's note and thus form same posterior belief in B
-# these users also change vote on A accordingly
-# algorithm should estimate posterior_a close to true posterior_a
+# Scenario:
+# ------------------------------------------------------------------------------
+# assumption 1: Users have common priors wrt A/B
+# assumption 2: Users vote honestly and trust that everyone votes honestly
+# ------------------------------------------------------------------------------
+# Post A: Is A true?
+# Post B: A is true
+# ------------------------------------------------------------------------------
+# Step 1: - All users downvote A because p_a is just under 50%
+# Step 2: - User U given true value of B
+#         - User U posts note saying B is true
+#         - Minority of users consider U's note and thus form same posterior
+#           belief in B
+# Step 3: - These users also change vote on A accordingly
+# ------------------------------------------------------------------------------
+# Expectation: Algorithm should estimate posterior_a close to true posterior_a
 
 function b_implies_a(step_func::Function, db::SQLite.DB, tag_id::Int)
+    root_post_id = 2
+    note_id = 3
+    A = SimulationPost(nothing, root_post_id, "Is A true?")
+    B = SimulationPost(root_post_id, note_id, "A is true")
+
     # common priors
-    p_a_given_b = .9
-    p_a_given_not_b = .01
-    p_b = .5
+    p_a_given_b = 0.9
+    p_a_given_not_b = 0.01
+    p_b = 0.5
 
     # Law of total probability
     p_a = p_b * p_a_given_b + (1 - p_b) * p_a_given_not_b
@@ -24,51 +34,42 @@ function b_implies_a(step_func::Function, db::SQLite.DB, tag_id::Int)
 
     n_users = 100
 
-    root_post_id = 2
-    note_id = 3
+    # --------------------------------------------------------------------------
+    # --- STEP 1 ---------------------------------------------------------------
+    # --------------------------------------------------------------------------
 
-    draws_0 = [p_a > 0.5 ? true : false for i in 1:n_users]
+    posts_0 = [A]
+    votes_0 = [
+        p_a > 0.5 ?
+            SimulationVote(nothing, A.post_id, 1, i) :
+            SimulationVote(nothing, A.post_id, -1, i)
+        for i in 1:n_users
+    ]
+    step_func(db, 1, posts_0, votes_0; tag_id = tag_id)
 
-    step_func(db, nothing, root_post_id, draws_0, 1; tag_id = tag_id)
+    # --------------------------------------------------------------------------
+    # --- STEP 2 ---------------------------------------------------------------
+    # --------------------------------------------------------------------------
 
     n_subset = 10
-    draws_1 = [posterior_b > 0.5 ? true : false for i in 1:n_subset]
-    draws_2 = [posterior_a > 0.5 ? true : false for i in 1:n_subset]
+    posts_1 = [B]
+    votes_1 = [
+        posterior_b > 0.5 ?
+            SimulationVote(root_post_id, note_id, 1, i) :
+            SimulationVote(root_post_id, note_id, -1, i)
+        for i in 1:n_subset
+    ]
+    step_func(db, 2, posts_1, votes_1; tag_id = tag_id)
 
-    step_func(db, root_post_id, note_id, draws_1, 2; tag_id = tag_id)
-    step_func(db, nothing, root_post_id, draws_2, 3; tag_id = tag_id)
+    # --------------------------------------------------------------------------
+    # --- STEP 2 ---------------------------------------------------------------
+    # --------------------------------------------------------------------------
 
+    votes_2 = [
+        posterior_a > 0.5 ?
+            SimulationVote(nothing, root_post_id, 1, i) :
+            SimulationVote(nothing, root_post_id, -1, i)
+        for i in 1:n_subset
+    ]
+    step_func(db, 3, SimulationPost[], votes_2; tag_id = tag_id)
 end
-
-
-# run_simulation(db, tag_id=2) do process_votes
-
-#     # common priors
-#     p_a_given_b = .9
-#     p_a_given_not_b = .01
-#     p_b = .5
-
-#     # Law of total probability
-#     p_a = p_b * p_a_given_b + (1 - p_b) * p_a_given_not_b
-
-
-#     posterior_b = 1
-#     posterior_a = p_a_given_b
-
-#     n_users = 100
-
-#     root_post_id = 1
-#     note_id = 2
-
-#     draws_0 = [p_a > 0.5 ? true : false for i in 1:n_users]
-
-#     process_votes(nothing, root_post_id, draws_0)
-
-#     n_subset = 10
-#     draws_1 = [posterior_b > 0.5 ? true : false for i in 1:n_subset]
-#     draws_2 = [posterior_a > 0.5 ? true : false for i in 1:n_subset]
-
-#     process_votes(root_post_id, note_id, draws_1)
-#     process_votes(nothing, root_post_id, draws_2)
-
-# end
