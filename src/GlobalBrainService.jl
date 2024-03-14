8710a5db37cd6908c8df8618bc922f7133eb28f5
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-03-14 12:41:37 +0100

    Adjust GlobalBrainService module to new structure

--- src/GlobalBrainService.jl
+++ src/GlobalBrainService.jl
@@ -8,9 +8,12 @@ using Dates
 include("GlobalBrain/GlobalBrain.jl")
 using .GlobalBrain
 
-include("ScoreDB/ScoreDB.jl")
-using .ScoreDB
+# Database
+include("types.jl")
+include("converters.jl")
+include("db.jl")
 
+# Service
 include("vote-events.jl")
 include("main.jl")
 
@@ -21,12 +24,35 @@ export global_brain_service
 export get_score_db
 export process_vote_events_stream
 
-include("simulations.jl")
 
+# Simulations
+include("simulations.jl")
 export get_sim_db
 export SimulationPost
 export SimulationVote
 export create_simulation_post!
 export run_simulation!
+
+# Database
+export SQLTalliesTree
+export Score
+export create_event
+export VoteEvent
+
+export to_detailed_tally
+export as_tallies_tree
+
+export get_score_db
+export init_score_db
+export get_tallies
+
+export insert_event
+export insert_event
+export set_last_processed_vote_event_id
+export get_last_processed_vote_event_id
+
+export insert_vote_event
+
+
 
 end
