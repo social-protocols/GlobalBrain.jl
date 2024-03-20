aeec213d443f80f0beb0de520dcac62cca8e9dd1
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-03-20 16:48:41 +0000

    Update flake, add dependencies for visualization app

--- flake.nix
+++ flake.nix
@@ -6,56 +6,59 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ ];
+          overlays = [];
           config = {
             allowUnfree = false;
-            packageOverrides = super: let self = super.pkgs; in
-            {
+            packageOverrides = super: let
+              self = super.pkgs;
+            in {
               rEnv = super.rWrapper.override {
                 packages = with self.rPackages; [
-                    shiny
-                    shinydashboard
-                    DBI
-                    RSQLite
-                    dplyr
-                    tidyr
-                    r2d3
-                    languageserver
+                  shiny
+                  shinydashboard
+                  DBI
+                  RSQLite
+                  dplyr
+                  tidyr
+                  r2d3
+                  languageserver
                 ];
               };
             };
           };
         };
-      in
-      {
+      in {
         devShells = {
-          default = with pkgs; pkgs.mkShellNoCC {
-            buildInputs = [
-              just
-              git
-              openssh # Necessary to run git on macs
-              julia-bin
-              # libgcc
-              sqlite-interactive
-              litecli
-              vim
-              neovim
-              less
-              fzf
-              cloc
-              entr
-              rEnv
-              yarn-berry
-            ];
-          };
+          default = with pkgs;
+            pkgs.mkShellNoCC {
+              buildInputs = [
+                just
+                git
+                openssh # Necessary to run git on macs
+                julia-bin
+                # libgcc
+                sqlite-interactive
+                litecli
+                vim
+                neovim
+                less
+                fzf
+                cloc
+                entr
+                rEnv
+                nodejs
+              ];
+            };
         };
       }
     );
 }
-
-
