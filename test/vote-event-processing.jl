53377404ee84c07c9e3bc22d19a86cc6e5cfdfb9
Jonathan R. Warden <john.warden@gmail.com>
2024-04-30 13:00:57 +0200

    Fix broken test/vote-event-processing.jl

--- test/vote-event-processing.jl
+++ test/vote-event-processing.jl
@@ -6,7 +6,12 @@
     end
 
     test_vote_event = """{"user_id":"100","tag_id":1,"parent_id":null,"post_id":1,"note_id":null,"vote":1,"vote_event_time":1708772663570,"vote_event_id":1}"""
-    result = process_vote_event_json(test_vote_event)
+    result = process_vote_event_json(ENV["DATABASE_PATH"], test_vote_event)
     @info result
     @test result.scoreEvents == """{\"vote_event_id\":1,\"vote_event_time\":1708772663570,\"score\":{\"tag_id\":1,\"post_id\":1,\"top_note_id\":null,\"o\":0.9129,\"o_count\":1,\"o_size\":1,\"p\":0.9129,\"score\":0.7928}}\n"""
+
+    if isfile(ENV["DATABASE_PATH"])
+        rm(ENV["DATABASE_PATH"])
+    end
+
 end
