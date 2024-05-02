415c77f8e7196387d67effe641352ec8ed44fafb
Felix Dietze <github@felx.me>
2024-05-02 17:28:58 +0200

    Revert "Upgrade PackageCompiler.jl"
    Revert "include_lazy_artifacts=true"
    
    This reverts commit 8006c15a2e93cd0148cd39d7dfc37e09dc167865.
    
    Revert "Remove unnecessary build step after PackageCompiler update"
    
    This reverts commit 17f19fd13b432ebabd6b1b459b4ed7c5fd63360a.
    
    This reverts commit a93b5c63f16fd6bd6d792f050197f45327e7d49a.
    
    Revert "Remove unnecessary step from build.jl for package compilation"
    
    This reverts commit 80da9d101afb1de3294a9ee14ff5940f352726f0.

--- globalbrain-node/julia/build.jl
+++ globalbrain-node/julia/build.jl
@@ -15,7 +15,8 @@ mkpath(builddir)
 # From PackageCompiler.jl `create_app(...)`
 @info "  bundle_artifacts"
 ctx = create_pkg_context(project)
-bundle_artifacts(ctx, builddir, include_lazy_artifacts=true)
+bundle_artifacts(ctx, builddir, include_lazy_artifacts=false)
+bundle_julia_libraries(builddir)
 
 cp(joinpath(project, "Project.toml"), joinpath(builddir, "lib", "Project.toml"))
 
