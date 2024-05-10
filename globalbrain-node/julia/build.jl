6d9b612abc0c2439cd9337ca8a2c45f94e2a6391
Felix Dietze <github@felx.me>
2024-05-10 14:04:03 +0200

    speed up build

--- globalbrain-node/julia/build.jl
+++ globalbrain-node/julia/build.jl
@@ -7,8 +7,8 @@ target_dir = "$(@__DIR__)/../globalbrain-compiled"
 println("Creating library in $target_dir")
 PackageCompiler.create_library(globalbrainSourceDir, target_dir;
                                 lib_name="globalbrain",
-                                incremental=false,
-                                filter_stdlibs=true,
+                                incremental=true,
+                                filter_stdlibs=false,
                                 force=true, # Overwrite target_dir.
                                 header_files = ["$(@__DIR__)/globalbrain.h"],
                             )
