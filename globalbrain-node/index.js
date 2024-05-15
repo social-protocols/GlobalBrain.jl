c1df1a5d7319d0c562f4b86375101cdbb893ab8e
Jonathan R. Warden <john.warden@gmail.com>
2024-05-15 17:18:05 +0200

    Upgrade node-pre-gyp. Move it and node-gyp to devDependencies

--- globalbrain-node/index.js
+++ globalbrain-node/index.js
@@ -1,4 +1,4 @@
-const binary = require('node-pre-gyp');
+const binary = require('node-gyp');
 const path = require('path');
 
 const bindingPath = binary.find(path.resolve(path.join(__dirname, './package.json')));
