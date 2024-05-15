7a5708362d488841b366f150c6cbaf175af6bbf6
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-05-15 12:21:25 +0200

    Fix formatting in shell test scripts

--- test/test-scoring.sh
+++ test/test-scoring.sh
@@ -12,7 +12,7 @@ rm -f $TEST_DB_FILENAME
 TMPDIR=`mktemp -d /tmp/global-brain-service-test.XXXXXX`; (
     set -e
     echo "Testing scoring algorithm";
-		julia --project --eval "using GlobalBrain; init_score_db(ARGS[1])" $TEST_DB_FILENAME
+    julia --project --eval "using GlobalBrain; init_score_db(ARGS[1])" $TEST_DB_FILENAME
     julia --project -- scripts/run.jl $TEST_DB_FILENAME $test_vote_events_json_file $TMPDIR/score-events.jsonl;
     echo "Comparing $TMPDIR/score-events.jsonl to $expected_score_events_file";
     diff -b $expected_score_events_file $TMPDIR/score-events.jsonl;
