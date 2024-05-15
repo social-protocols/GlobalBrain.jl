0fd7dfc160bfc9a2e1e8226e91bf4b927f97600e
Jonathan R. Warden <john.warden@gmail.com>
2024-05-15 23:10:21 +0200

    Remove node-pre-gyp from build. Upgrade node-gyp and node-addon-api

--- globalbrain-node/test.js
+++ globalbrain-node/test.js
@@ -1,13 +1,13 @@
-const global_brain = require('.');
+const bindingPath = require.resolve(`./build/Release/binding`);
+const global_brain = require(bindingPath);
 
-const test_db_path = process.argv[2]
+const test_db_path = process.argv[2] || 'test.db'
+console.log("Using test db ", test_db_path)
 
-// delete test_db_path if it exists
 const fs = require('fs');
 if (fs.existsSync(test_db_path)) {
 	fs.unlinkSync(test_db_path);
 }
-
 
 const test_vote_event = `{"user_id":"100","tag_id":1,"parent_id":null,"post_id":1,"note_id":null,"vote":1,"vote_event_time":1708772663570,"vote_event_id":1}`
 console.log(global_brain.process_vote_event_json(test_db_path, test_vote_event));
@@ -19,5 +19,5 @@ const test_vote_event3 = `{"user_id":"101","tag_id":1,"parent_id":null,"post_id"
 console.log(global_brain.process_vote_event_json(test_db_path, test_vote_event3));
 
 if (fs.existsSync(test_db_path)) {
-	fs.unlinkSync(test_db_path);
+       fs.unlinkSync(test_db_path);
 }
