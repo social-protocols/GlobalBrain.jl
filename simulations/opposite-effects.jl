a777b1c18a752728206075733de24eb99c13fa52
Jonathan R. Warden <john.warden@gmail.com>
2024-05-27 16:07:39 +0200

    Create simulations/opposite-effects.jl

--- simulations/opposite-effects.jl
+++ simulations/opposite-effects.jl
@@ -1,118 +1,95 @@
-# # 3 posts: parent post and two children with opposite effects
-# # -- all posts are posted
-# # -- then people vote on A
-# # -- then people vote on B and C
-# # -- @test -> decide
+(sim::SimulationAPI) -> begin
+    Random.seed!(3); 
 
-# function opposite_effects(sim::SimulationAPI)
-#     Random.seed!(3); 
+    A = sim.post!(nothing, "A")
 
-#     A = sim.post!(nothing, "A")
-#     B = sim.post!(A.post_id, "B")
-#     C = sim.post!(A.post_id, "C")
+    p_a = .5
+    p_a_given_b = .9
+    p_a_given_c = .1
+    p_a_given_b_and_c = .6
 
 
-#     p_a_given_b = 0.9
-#     p_a_given_c = 0.1
+    # p_a = .5
+    # p_a_given_c = .5
+    # p_a_given_b = .9
+    # p_a_given_b_and_c = .5
 
-#     p_a = 0.5
+    group0 = 1:100
+    # Group of users that vote only on B
+    group1 = 101:150
+    # Group of users that vote on both B and C
+    group_overlap = 141:160
+    # Group of users that vote only on C
+    group2 = 151:200
 
-#     p_b = 0.9
-#     p_c = 0.9
+    # Step 1: Group 0 only votes on A
+    begin
+        votes = [
+            SimulationVote(A.post_id, rand(Bernoulli(p_a)) == 1 ? 1 : -1, i)
+            for i in group0
+        ]
 
+        (scores, effects) = sim.step!(1, votes; description="Among users that only consider A, $(p_a*100)% agree with A.")
 
-#     # # common priors
-#     # p_a_given_b = .9
-#     # p_a_given_not_b = .01
-#     # p_b = .5
+        p = scores[A.post_id].p
 
-#     # # Law of total probability
-#     # p_a = p_b * p_a_given_b + (1 - p_b) * p_a_given_not_b
+        @testset "Step 1: Initial beliefs ($p ≈ $p_a)" begin
+            @test p ≈ p_a atol = 0.1
+        end
+    end
 
-#     # p_a_given_c = p_a + (p_a_given_b-p_a)*.4
+    # Step 2: Group 1 votes on A and B.
+    begin
+        B = sim.post!(A.post_id, "B")
+        # votes_b = votesGivenBeliefs(B.post_id, repeat([posterior_b], n_group1))
+        votes_b = [
+            SimulationVote(B.post_id, 1, i)
+            for i in group2
+        ]
 
-#     # posterior_b = 1
-#     # posterior_a = p_a_given_b
+        votes_a = [ 
+            # SimulationVote(B.post_id, 1, i)
+            SimulationVote(A.post_id, rand(Bernoulli(p_a_given_b)) == 1 ? 1 : -1, i) 
+            for i in group2 
+        ]
 
-#     n_users = 160 
+        all_votes = [votes_a; votes_b]
 
-#     # Step 1: All users vote on A
-#     begin
+        (scores, effects) = sim.step!(2, all_votes; description="Among users that consider B, $(p_a_given_b*100)% agree with A.")
 
+        p = scores[A.post_id].p
 
-
-#         votes = [
-#             SimulationVote(A.post_id, rand(Bernoulli(p_a)) == 1 ? 1 : -1, i)
-#             for i in 1:n_users
-#         ]
-
-#         # votes = votesGivenBeliefs(A.post_id, repeat([p_a], n_users))
-#         scores = sim.step!(1, votes, description="There are $n_users users. $(p_a*100)% agree with A.")
-
-#         p = scores[A.post_id].p
-
-#         @testset "Step 1: Initial beliefs ($p ≈ $p_a)" begin
-#             @test p ≈ p_a atol = 0.1
-#         end
-#     end
-
-
-#     # Step 2
-#     # First n_subset1 users vote on B
-#     n_subset1 = 40
-#     begin
-#         B = sim.post!(A.post_id, "B")
-#         # votes_b = votesGivenBeliefs(B.post_id, repeat([posterior_b], n_subset1))
-#         votes_b = [
-#             SimulationVote(B.post_id, rand(Bernoulli(posterior_b)) == 1 ? 1 : -1, i)
-#             for i in 1:n_subset1
-#         ]
-
-#         votes_a = [
-#             SimulationVote(A.post_id, rand(Bernoulli(posterior_a)) == 1 ? 1 : -1, i)
-#             for i in 1:n_subset1
-#         ]
-
-#         scores = sim.step!(2, [votes_a; votes_b], description="Of the $n_subset1 users that consider B, $(p_a_given_b*100)% agree with A.")
-
-#         p = scores[A.post_id].p
-
-#         @testset "Step 2: B changes mind  ($p ≈ $posterior_a)" begin
-#             # should approach 1 but given small sample size still be a bit below 
-#             @test p ≈ posterior_a atol = 0.1
-#         end
-#     end
+        @testset "Step 2: B changes mind ($p ≈ $p_a_given_b)" begin
+            @test p ≈ p_a_given_b atol = 0.1
+        end
+    end
 
 
-#     # Step 3
-#     # Second 50 users vote on C
-#     n_subset2 = 30
-#     begin
-#         p_C = 1
-#         C = sim.post!(A.post_id, "C")
-#         # votes_c = votesGivenBeliefs(C.post_id, repeat([p_C], n_subset2); skip_users=n_subset1)
-#         votes_c = [
-#             SimulationVote(C.post_id, rand(Bernoulli(p_C)) == 1 ? 1 : -1, i)
-#             for i in n_subset1+1:(n_subset1+n_subset2)
-#         ]
+    # Step 3: Group 2 and the overlap group vote in A and C 
+    begin
+        C = sim.post!(A.post_id, "C")
+        votes_c1 = [ SimulationVote(C.post_id, 1, i) for i in group2 ]
+        votes_c2 = [ SimulationVote(C.post_id, 1, i) for i in group_overlap ]
 
-#         # And change vote on A. But C didn't change minds
-#         # votes_a = votesGivenBeliefs(A.post_id, repeat([p_a_given_c], n_subset2); skip_users=n_subset1)
-#         votes_a = [
-#             SimulationVote(A.post_id, rand(Bernoulli(p_a_given_c)) == 1 ? 1 : -1, i)
-#             for i in n_subset1+1:(n_subset1+n_subset2)
-#         ]
+        votes_a_given_c = [
+            SimulationVote(A.post_id, rand(Bernoulli(p_a_given_c)) == 1 ? 1 : -1, i)
+            for i in group2
+        ]
 
-#         scores = sim.step!(3, [votes_a; votes_c]; description="Among a separate group of $n_subset2 users that consider C, $(round(p_a_given_c*100, digits=2))% agree with A. Since C has a smaller effect on A than does B, it does not become the critical response and does not effect the informed upvote probability estimate for A.")
+        votes_a_given_b_and_c = [
+            SimulationVote(A.post_id, rand(Bernoulli(p_a_given_b_and_c)) == 1 ? 1 : -1, i)
+            for i in group_overlap
+        ]
 
-#         p1 = scores[A.post_id].p
+        all_votes = [votes_c1; votes_c2; votes_a_given_c; votes_a_given_b_and_c]
 
-#         @testset "Step 2: C doesn't change minds ($p ≈ $p_a_given_c)" begin
-#             # should approach 1 but given small sample size still be a bit below 
-#             @test p1 ≈ p atol = 0.1
-#         end
-#     end
+        (scores, effects) = sim.step!(3, all_votes; description="Among users who only consider C, $(round(p_a_given_c*100, digits=2))% agree with A. Among users who consider B and C, $(round(p_a_given_b_and_c*100, digits=2))% agree with A.")
 
-# end
+        p1 = scores[A.post_id].p
 
-function opposite_effects() end
+        @testset "Step 3: C also changes minds. The combined effect of B and C is weakly positive. ($p ≈ $p_a_given_b_and_c)" begin
+            @test_broken p1 ≈ p_a_given_b_and_c atol = 0.1
+        end
+    end
+
+end
