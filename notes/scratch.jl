3fa8d2a47dc89cf6710f4d0b367c96a9ba86a352
Jonathan R. Warden <john.warden@gmail.com>
2024-06-03 18:10:07 +0200

    Remove unused notes/scratch.jl

--- notes/scratch.jl
+++
@@ -1,8 +0,0 @@
-
-# todo:
-#   - reply events (sepearte reply field)
-#   - don't write vote events to Tables
-#   - 'shown' events should be shown and not voted/replied
-#   - "exists" and not shown event?
-#   - json
-#   - tests for aggregates. SQL alternative?
