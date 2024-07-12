3963f02dedde1c067b58b2588f16ecaec69504f8
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-07-12 16:06:53 +0200

    Move remaining score.jl functions into algorithm.jl

--- src/lib/algorithm.jl
+++ src/lib/algorithm.jl
@@ -1,3 +1,19 @@
+# The global prior upvote probability is Beta(.25, .25), or a beta distribution with mean 0.5 and weight 0.5. 
+# See reasoning in: https://github.com/social-protocols/internal-wiki/blob/main/pages/research-notes/2024-06-03-choosing-priors.md#user-content-fnref-1-35b0437c85b2f65e7c3d7139bba82f66
+const GLOBAL_PRIOR_UPVOTE_PROBABILITY_SAMPLE_SIZE = C1 = 0.50
+const GLOBAL_PRIOR_UPVOTE_PROBABILITY_MEAN = 0.5
+const GLOBAL_PRIOR_UPVOTE_PROBABILITY = BetaDistribution(
+    GLOBAL_PRIOR_UPVOTE_PROBABILITY_MEAN,
+    GLOBAL_PRIOR_UPVOTE_PROBABILITY_SAMPLE_SIZE,
+)
+
+# The global prior weight for the *informed* upvote probability is just a guess, based on
+# the belief that the prior weight for the informed upvote probability should be higher than
+# that for the uninformed upvote probability. A priori, arguments do not change minds.
+const GLOBAL_PRIOR_INFORMED_UPVOTE_PROBABILITY_SAMPLE_SIZE = C2 = 5.0
+
+
+
 function score_posts(output_event::Function, posts::Vector{TalliesTree})
     effects = Dict{Int,Vector{Effect}}()
     for post in posts
@@ -106,4 +122,38 @@ function add!(effects::Dict{Int,Vector{Effect}}, new_effect::Effect)
         effects[new_effect.comment_id] = []
     end
     push!(effects[new_effect.comment_id], new_effect)
+end
+
+function calc_uninformed_probability(
+    prior::BetaDistribution,
+    tally::ConditionalTally,
+)::Number
+    return prior |>
+        (x -> reset_weight(x, GLOBAL_PRIOR_INFORMED_UPVOTE_PROBABILITY_SAMPLE_SIZE)) |>
+        (x -> update(x, tally.uninformed)) |>
+        (x -> x.mean)
+end
+
+function partially_informed_probability_dist(
+    prior::BetaDistribution,
+    tally::ConditionalTally,
+)::BetaDistribution
+    return prior |>
+        (x -> reset_weight(x, GLOBAL_PRIOR_INFORMED_UPVOTE_PROBABILITY_SAMPLE_SIZE)) |>
+        (x -> update(x, tally.informed))
+end
+
+
+function effect_score(effect::Effect)::Float64
+    return information_gain(effect.p, effect.q, effect.r)
+end
+
+function direct_score(p)::Float64
+    p * (1 + log2(p))
+end
+
+# The total ranking score for a post includes the direct score for
+# the post itself, plus the value of its effects on other posts.
+function ranking_score(effects::Vector{Effect}, p::Float64)::Float64
+    return direct_score(p) + sum([effect_score(e) for e in effects])
 end
