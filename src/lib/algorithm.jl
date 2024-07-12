3c0300c48c027c172e4280e9c03289c305f51f09
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-07-12 17:51:38 +0200

    Refactor algorithm
    - remove trampoline recursion
    - more descriptive names

--- src/lib/algorithm.jl
+++ src/lib/algorithm.jl
@@ -14,106 +14,83 @@ const GLOBAL_PRIOR_UPVOTE_PROBABILITY = BetaDistribution(
 const GLOBAL_PRIOR_INFORMED_UPVOTE_PROBABILITY_SAMPLE_SIZE = C2 = 5.0
 
 
-function score_posts(output_event::Function, posts::Vector{TalliesTree})
-    effects = Dict{Int,Vector{Effect}}()
-    for post in posts
-        score_post(output_event, post, effects)
-    end
-end
-
-
-function score_post(output_event::Function, post::TalliesTree, effects::Dict{Int,Vector{Effect}})
-    post_id = post.post_id
+function score_tree_and_emit_events(tree::TalliesTree, emit_event::Function, effects::Dict{Int,Vector{Effect}})
+    post_id = tree.post_id
     @debug "In score post $post_id"
 
-    if !post.needs_recalculation
+    if !tree.needs_recalculation
         @debug "No recalculation needed for $(post_id). Using existing score"
         return Vector{Score}()
     end
 
-    o = GLOBAL_PRIOR_UPVOTE_PROBABILITY |> (x -> update(x, post.tally))
+    o = GLOBAL_PRIOR_UPVOTE_PROBABILITY |> (x -> update(x, tree.tally))
     @debug "Overall probability in score_post for $post_id is $(o.mean)"
 
-    p = calc_informed_probability(post_id, post, o, effects)
+    p = calc_informed_probability_and_effects(
+        post_id,
+        tree,
+        o,
+        (effect) -> begin
+            add!(effects, effect)
+            emit_event(effect)
+        end,
+    )
 
-    my_effects::Vector{Effect} = get(effects, post_id, [])
-    for e in my_effects
-        output_event(e)
-    end
-
-    for child in post.children()
-        score_post(output_event, child, effects)
+    for subtree in tree.children()
+        score_tree_and_emit_events(subtree, emit_event, effects) # recursion
     end
 
     score = Score(
         post_id = post_id,
         o = o.mean,
-        o_count = post.tally.count,
-        o_size = post.tally.size,
+        o_count = tree.tally.count,
+        o_size = tree.tally.size,
         p = p,
-        score = ranking_score(my_effects, p),
+        score = ranking_score(post_id, effects, p),
     )
-    output_event(score)
+    emit_event(score)
 end
 
 
-function calc_informed_probability(
-    post_id::Int,
-    target::TalliesTree,
+function calc_informed_probability_and_effects(
+    target_id::Int,
+    subtree::TalliesTree,
     r::BetaDistribution,
-    effects::Dict{Int,Vector{Effect}},
+    effect_callback::Function,
 )::Number
-    comment_id = target.post_id
+    child_effects = map(
+        child -> begin
+            effect =
+                if !child.needs_recalculation
+                    child.effect(target_id) # TODO: should an event also be emitted for this effect?
+                else
+                    tally = child.conditional_tally(target_id)
+                    r_dist = partially_informed_probability_dist(r, tally)
 
-    @debug "weighted_average_informed_probability $post_id=>$(comment_id), r=$(r.mean)"
+                    Effect(
+                        post_id = target_id,
+                        comment_id = child.post_id,
+                        p = calc_informed_probability_and_effects(target_id, child, r_dist, effect_callback), # recursion
+                        q = calc_uninformed_probability(r, tally),
+                        r = r_dist.mean,
+                        conditional_tally = tally,
+                    )
+                end
+            effect_callback(effect)
+            return effect
+        end,
+        subtree.children()
+    )
 
-    children = target.children()
-    @debug "Got $(length(children)) children for $comment_id"
+    return weighted_average_informed_probability(child_effects, r)
+end
 
-    @debug "Getting effects of children of $comment_id on $post_id"
-    child_effects =
-        [calc_thread_effect(post_id, child, r, effects) for child in children] |>
-        (x -> filter(effect -> effect.weight > 0, x))
 
-    if length(child_effects) == 0
+function weighted_average_informed_probability(child_effects::Vector{Effect}, r::BetaDistribution)::Number
+    total_weight = sum([effect.weight for effect in child_effects])
+    if total_weight == 0
         return r.mean
     end
-
-    return weighted_average_informed_probability(child_effects)
-end
-
-
-function calc_thread_effect(
-    post_id::Int,
-    target::TalliesTree,
-    prior::BetaDistribution,
-    effects::Dict{Int,Vector{Effect}},
-)::Effect
-    if !target.needs_recalculation
-        return target.effect(post_id)
-    end
-
-    tally = target.conditional_tally(post_id)
-    r_dist = partially_informed_probability_dist(prior, tally)
-
-    effect = Effect(
-        post_id = post_id,
-        comment_id = target.post_id,
-        p = calc_informed_probability(post_id, target, r_dist, effects),
-        q = calc_uninformed_probability(prior, tally),
-        r = r_dist.mean,
-        conditional_tally = tally,
-    )
-    add!(effects, effect)
-
-    @debug "p=$(effect.p) for $post_id=>$(target.post_id)"
-
-    return effect
-end
-
-
-function weighted_average_informed_probability(child_effects::Vector{Effect})::Number
-    total_weight = sum([effect.weight for effect in child_effects])
     weighted_sum = sum([effect.weight * effect.p for effect in child_effects])
     return weighted_sum / total_weight
 end
@@ -150,8 +127,8 @@ end
 
 # The total ranking score for a post includes the direct score for
 # the post itself, plus the value of its effects on other posts.
-function ranking_score(effects::Vector{Effect}, p::Float64)::Float64
-    return direct_score(p) + sum([effect_score(e) for e in effects])
+function ranking_score(post_id::Number, effects::Vector{Effect}, p::Float64)::Float64
+    return direct_score(p) + sum([effect_score(e) for e in get(effects, post_id, [])])
 end
 
 
